793be336c499beab5f38087df039aedc17785dd2
nvaro <varonono@yahoo.com>
2018-08-03 09:32:48 +0300

    music app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { SongService } from './song.service';
+import { AdminComponent } from './components/admin/admin.component';
+import {AppRoutingModule} from './app-routing.module';
+import { AlertModule } from 'ngx-bootstrap';
+import { SongComponent } from './song/song.component';
+import { AddComponent } from './add/add.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    SongComponent,
+    AddComponent
   ],
   imports: [
-    BrowserModule
+    AlertModule.forRoot(),
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SongService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
